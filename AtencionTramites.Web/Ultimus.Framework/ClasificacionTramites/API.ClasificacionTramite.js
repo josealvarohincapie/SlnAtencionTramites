6066d83fc2c64bb09b80d8484024e30d47aaf43c
Mateo Polanco Rodriguez <mateopolanco2@gmail.com>
2024-03-08 10:19:37 -0500

    Avance en el js de Clasificacion de tramites

--- AtencionTramites.Web/Ultimus.Framework/ClasificacionTramites/API.ClasificacionTramite.js
+++ AtencionTramites.Web/Ultimus.Framework/ClasificacionTramites/API.ClasificacionTramite.js
@@ -1,8 +1,30 @@
-﻿jQuery(document).ready(function () {
+﻿$("#frmInfoRadicado").EnableValidationToolTip();
+$("#frmAnexosRadicado").EnableValidationToolTip();
+$("#frmRegistroPeticionarios").EnableValidationToolTip();
+$("#frmClasificacionPeticion").EnableValidationToolTip();
+$("#frmConclusionPeticion").EnableValidationToolTip();
+
+jQuery(document).ready(function () {
     UpdateControlsSettings("FILTRO_FRM_BotonesAT", "BotonesAT", "frmBotones", function () {
-       getDataModel();
+        UpdateControlsSettings("FILTRO_FRM_AnexosRadicadoAT", "AnexosRadicadoAT", "frmAnexosRadicado", function () {
+            UpdateControlsSettings("FILTRO_FRM_BotonesAT", "BotonesAT", "frmBotones", function () {
+                UpdateControlsSettings("FILTRO_FRM_ClasificacionPeticionAT", "ClasificacionPeticionAT", "frmClasificacionPeticion", function () {
+                    UpdateControlsSettings("FILTRO_FRM_DecisionAT", "DecisionAT", "frmConclusionPeticion", function () {
+                        UpdateControlsSettings("FILTRO_FRM_InfoRadicadoAT", "InfoRadicadoAT", "frmInfoRadicado", function () {
+                            UpdateControlsSettings("FILTRO_FRM_PeticionarioEnfoqueAT", "PeticionarioEnfoqueAT", "frmRegistroPeticionarios", function () {
+                                UpdateControlsSettings("FILTRO_FRM_PeticionariosAT", "PeticionariosAT", "frmRegistroPeticionarios", function () {
+                                    UpdateControlsSettings("FILTRO_FRM_PeticionarioTablaAT", "PeticionarioTablaAT", "frmRegistroPeticionarios", function () {
+                                        getDataModel();
+                                    });
+                                });
+                            })
+                        });
+                    });
+                });
+            });
+        });
     });
-})
+});
 /*
 FILTRO_FRM_AnexosRadicadoAT	        FILTRO_FRM_AnexosRadicadoAT	 
 FILTRO_FRM_BotonesAT	            FILTRO_FRM_BotonesAT	 
