7e6c271114ea3692b4a7fc04fc73cbba480efaab
Jose Alvaro hincapie Castillo <alvarohincapie@gmail.com>
2024-03-08 09:54:20 -0500

    Se adicionan filtro

--- AtencionTramites.Web/Ultimus.Framework/ClasificacionTramites/API.ClasificacionTramite.js
+++ AtencionTramites.Web/Ultimus.Framework/ClasificacionTramites/API.ClasificacionTramite.js
@@ -3,6 +3,16 @@
        getDataModel();
     });
 })
+/*
+FILTRO_FRM_AnexosRadicadoAT	        FILTRO_FRM_AnexosRadicadoAT	 
+FILTRO_FRM_BotonesAT	            FILTRO_FRM_BotonesAT	 
+FILTRO_FRM_ClasificacionPeticionAT	FILTRO_FRM_ClasificacionPeticionAT	 
+FILTRO_FRM_DecisionAT	|           FILTRO_FRM_DecisionAT	 
+FILTRO_FRM_InfoRadicadoAT	        FILTRO_FRM_InfoRadicadoAT	 
+FILTRO_FRM_PeticionarioEnfoqueAT	FILTRO_FRM_PeticionarioEnfoqueAT	 
+FILTRO_FRM_PeticionariosAT	        FILTRO_FRM_PeticionariosAT	 
+FILTRO_FRM_PeticionarioTablaAT	    FILTRO_FRM_PeticionarioTablaAT
+*/
 
 this.getDataModel = function () {
     ENDREQUEST();
